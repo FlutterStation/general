6b6b047024130a0af320263d23aa714e6a27d23c
KarlJan Reginaldo <74361344+karlreginaldo@users.noreply.github.com>
2022-05-29 19:12:20 +0800

    renamed into standard_spacing

--- lib/general.dart
+++ lib/general.dart
@@ -17,5 +17,5 @@ export 'service/print.dart';
 export 'theme/colortheme.dart';
 export 'theme/texttheme.dart';
 export 'widgets/button.dart';
-export 'widgets/scaffold_margin.dart';
+export 'widgets/standard_spacing.dart';
 export 'widgets/text.dart';
---
+++ lib/widgets/standard_spacing.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class StandardMargin extends StatelessWidget {
+  const StandardMargin(
+      {Key? key, this.child, this.vertical = 8, this.horizontal = 15})
+      : super(key: key);
+  final Widget? child;
+  final double vertical;
+  final double horizontal;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical),
+      child: child,
+    );
+  }
+}
+
+class StandardPadding extends StatelessWidget {
+  const StandardPadding(this.child,
+      {Key? key, this.vertical = 8, this.horizontal = 15})
+      : super(key: key);
+  final Widget child;
+  final double vertical;
+  final double horizontal;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical),
+      child: child,
+    );
+  }
+}
