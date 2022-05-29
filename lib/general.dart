159a1f23bd1d48a1ecab1cd0f151b1b4b609c04a
KarlJan Reginaldo <74361344+karlreginaldo@users.noreply.github.com>
2022-05-29 19:00:30 +0800

    add export for scaffold_margin.dart

--- lib/general.dart
+++ lib/general.dart
@@ -17,4 +17,5 @@ export 'service/print.dart';
 export 'theme/colortheme.dart';
 export 'theme/texttheme.dart';
 export 'widgets/button.dart';
+export 'widgets/scaffold_margin.dart';
 export 'widgets/text.dart';
