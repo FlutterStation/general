901f25910f1b02a0046b6ccda91a314b41fa04bc
KarlJan Reginaldo <74361344+karlreginaldo@users.noreply.github.com>
2022-05-30 10:50:04 +0800

    add validator ref

--- lib/general.dart
+++ lib/general.dart
@@ -14,8 +14,10 @@ import 'signature.dart';
 ///
 /// --------------------------
 export 'service/print.dart';
+export 'service/text_field_validator.dart';
 export 'theme/colortheme.dart';
 export 'theme/texttheme.dart';
 export 'widgets/button.dart';
 export 'widgets/standard_spacing.dart';
 export 'widgets/text.dart';
+export 'widgets/textfield.dart';
